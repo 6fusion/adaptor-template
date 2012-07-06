c42807ee87c9823705d86efc3ce342d41b33a16a
Chris Altman <chris@altonymous.com>
2012-07-06 16:49:51 -0400

    Fixed inodes#show route bug.

--- app/controllers/i_nodes.rb
+++ app/controllers/i_nodes.rb
@@ -2,14 +2,15 @@ AdaptorTemplate.controllers :inodes, :priority => :low do
   # Creates
   post :index do
     logger.info('i_nodes#index')
+
     i_node = INode.new(params)
     i_node.save
   end
 
-  get :show do
+  get :show, "/inodes/:uuid" do
     logger.info('i_nodes#show')
-    @i_node = INode.find_by_uuid(params[:uuid])
 
+    @i_node = INode.find_by_uuid(params[:uuid])
     render 'i_nodes/show'
   end
 
