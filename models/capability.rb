88db7d1150d610a4a98bf9c7b829aa6d39af8602
Kevin Olbrich <kolbrich@6fusion.com>
2012-07-12 08:27:54 -0400

    add information about capabilities, could use some better formatting

--- models/capability.rb
+++ models/capability.rb
@@ -1,21 +1,51 @@
 # @api public
+#  Possible capabilities include:
+#
+#  |--------------------+----------------------------------|
+#  | capability         | requires                         |
+#  |:-------------------|:---------------------------------|
+#  | power_on           | guest_inventory                  |
+#  | power_off          | guest_inventory                  |
+#  | unplug             | guest_inventory                  |
+#  | restart            | guest_inventory                  |
+#  | suspend            | guest_inventory                  |
+#  | resume             | guest_inventory                  |
+#  | guest_state        | none                             |
+#  | guest_inventory    | guest_state                      |
+#  | host_state         | none                             |
+#  | host_inventory     | host_state                       |
+#  | guest_reading      | none                             |
+#  | guest_metering     | guest_inventory, guest_reading   |
+#  | host_reading       | none                             |
+#  | host_metering      | host_inventory, host_reading     |
+#  | create             | guest_inventory                  |
+#  | delete             | guest_inventory                  |
+#  | clone              | guest_inventory                  |
+#  | edit               | guest_inventory                  |
+#  | move               | guest_inventory & host_inventory |
+#  | take_snapshot      | guest_inventory                  |
+#  | revert_to_snapshot | guest_inventory                  |
+#  | assign_ip          | guest_inventory                  |
+#  | unassign_ip        | guest_inventory                  |
+#  | import             | create                           |
+#  | export             | guest_inventory                  |
+#  |--------------------+----------------------------------|
+#
+#  Each of these capabilities may be implemented individually so long as the required
+#  capabilities have also been implemented.
 class Capability < Base::Capability
+
+  SUPPORTED_CAPABILITIES = ['guest_inventory',
+                            'guest_metering']
+
   # Should return a list of capabilities for an iNode
   #
   # @param [INode] i_node iNode instance that defines where the action is to take place
   # @return [Array<Capability>]
   def self.all(i_node)
     logger.info('Capability.all')
-    capabilities = Array.new
-
-    1.upto(5) do |i|
-      capability = Capability.new(
-        name: "#{i} Capability on #{i_node.connection}"
-      )
-
-      capabilities << capability
+    SUPPORTED_CAPABILITIES.map do |capability|
+      Capability.new(name: capability)
     end
-
-    capabilities
   end
 end
