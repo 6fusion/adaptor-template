76c3ed01a4f96190713c3391e69488c8e349a03f
Kevin Olbrich <kolbrich@6fusion.com>
2012-08-04 09:06:44 -0400

    allow overriding of setters on models during creation by hash

--- models/base/main.rb
+++ models/base/main.rb
@@ -6,7 +6,9 @@ class Main
   include ActiveSupport::JSON
 
   def initialize(args)
-    args.each do |k, v|
+    if self.respond_to?("#{k}=".to_sym)
+      self.send("#{k}=", v)
+    else
       instance_variable_set("@#{k}", v) unless v.nil?
     end
   end
